b2519cd7936c9ce21943e60692269bc0d2047ce2
Sai Manasa <gsaimanasa97@gmail.com>
2021-07-26 08:30:26 +0530

    added input and price details

--- PriceTracker.py
+++ PriceTracker.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-URL = 'https://www.amazon.in/dp/B087JY84QQ/ref=s9_acss_bw_pg_test_2_t?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-5&pf_rd_r=6SCA01Y8WS8DACSA114A&pf_rd_t=101&pf_rd_p=7a1d8318-cf85-4bbe-a69b-0064cd342a79&pf_rd_i=22817284031'
+URL = input("Enter product URL to get price:\n")
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
@@ -14,26 +14,26 @@ def check_price():
     soup = BeautifulSoup(page.content,'html.parser')
 
     title =  soup.find(id = 'productTitle').get_text()
-    price = soup.find(id= "priceblock_ourprice").get_text()
+    price = soup.find(id= "priceblock_dealprice").get_text()
     converted_price = float((price[1:7]).replace(',',''))
     if converted_price < 27000:
-        send_mail()
+        send_mail(converted_price)
         print(converted_price)
         print(title.strip())
 
-def send_mail():
+def send_mail(converted_price):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
-    server.login('***@gmail.com','****')
+    server.login('****@gmail.com','****')
     subject = 'Price fell down'
-    body = 'Check the link https://www.amazon.in/dp/B087JY84QQ/ref=s9_acss_bw_pg_test_2_t?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-5&pf_rd_r=6SCA01Y8WS8DACSA114A&pf_rd_t=101&pf_rd_p=7a1d8318-cf85-4bbe-a69b-0064cd342a79&pf_rd_i=22817284031'
+    body = 'Check the link'+URL
+    new_price = converted_price
+    msg =f"Subject:{subject}\n\n{body}\n\nPrice: Rs.{new_price}"
 
-    msg =f"Subject:{subject}\n\n{body}"
-
-    server.sendmail('****@gmail.com','****@gmail.com',msg)
+    server.sendmail('*****@gmail.com','*****@gmail.com',msg)
     print('HEY EMAIL HAS BEEN SENT!')
 
     server.quit()
@@ -41,3 +41,4 @@ def send_mail():
 while(True):
     check_price()
     time.sleep(60*60)
+    
